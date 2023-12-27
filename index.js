dc1a9b8f50ee2d231c48c0acd4523bb8edf597a2
Samar Abbas <samarabbas172003@gmail.com>
2023-12-27 17:41:06 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// window.onscroll = function () {
+//   change_Nav_onScroll();
+// };
+// window.onscrollend = function () {
+//   back_to_same_nav();
+// };
+
+// function back_to_same_nav() {
+//   document.getElementById("scroll").style.display = "none";
+// }
+// function change_Nav_onScroll() {
+//   const a = document.getElementById("scroll");
+
+//   document.getElementById("scroll").style.backgroundColor = "transparent";
+// }
